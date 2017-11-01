3cac6a88c676faebfe86c6ac0cce1eca91995f0d
tejaskb04 <tejaskb@gmail.com>
2017-11-01 12:49:48 -0700

    Setup

--- Horizan/app/src/main/java/com/horizan/horizan/LaunchActivity.java
+++ Horizan/app/src/main/java/com/horizan/horizan/LaunchActivity.java
@@ -1,13 +1,41 @@
 package com.horizan.horizan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LaunchActivity extends AppCompatActivity {
+
+    private Button registerBtn;
+    private Button loginBtn;
+    private Button dashboardBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        registerBtn = (Button) findViewById(R.id.register);
+        loginBtn = (Button) findViewById(R.id.login);
+        dashboardBtn = (Button) findViewById(R.id.dashboard);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LaunchActivity.this, RegisterActivity.class));
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LaunchActivity.this, LoginActivity.class));
+            }
+        });
+        dashboardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LaunchActivity.this, DashboardActivity.class));
+            }
+        });
     }
 }
