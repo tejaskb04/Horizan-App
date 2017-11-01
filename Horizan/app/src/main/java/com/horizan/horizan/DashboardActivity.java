4e8d35695af614f28a510c33063a33818213733e
tejaskb04 <tejaskb@gmail.com>
2017-11-01 13:09:48 -0700

    Bottom Navigation Bar Logic

--- Horizan/app/src/main/java/com/horizan/horizan/DashboardActivity.java
+++ Horizan/app/src/main/java/com/horizan/horizan/DashboardActivity.java
@@ -1,9 +1,35 @@
 package com.horizan.horizan;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_dashboard: {
+                    return true;
+                }
+                case R.id.navigation_search: {
+                    startActivity(new Intent(DashboardActivity.this, SearchActivity.class));
+                    return true;
+                }
+                case R.id.navigation_settings: {
+                    startActivity(new Intent(DashboardActivity.this, SettingsActivity.class));
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
