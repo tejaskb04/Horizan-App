c6e0c2aa2845d99194778beff71c733e4021d858
tejaskb04 <tejaskb@gmail.com>
2017-11-05 18:04:32 -0800

    Register/Login Works

--- Horizan/app/src/main/java/com/horizan/horizan/LoginActivity.java
+++ Horizan/app/src/main/java/com/horizan/horizan/LoginActivity.java
@@ -1,13 +1,77 @@
 package com.horizan.horizan;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private LinearLayout loginLayout;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginBtn;
+    private TextView registerRouteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginLayout = (LinearLayout) findViewById(R.id.login_layout);
+        emailEditText = (EditText) findViewById(R.id.email);
+        passwordEditText = (EditText) findViewById(R.id.password);
+        loginBtn = (Button) findViewById(R.id.login);
+        registerRouteTextView = (TextView) findViewById(R.id.register_route);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "There are one or more empty fields.",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                finish();
+                                startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Login Failed",
+                                        Toast.LENGTH_SHORT).show();
+
+                                // DEBUG CODE BELOW
+                                System.out.println(task.getException());
+                                //
+                            }
+                        }
+                    });
+        }
     }
 }
--- Horizan/app/src/main/java/com/horizan/horizan/RegisterActivity.java
+++ Horizan/app/src/main/java/com/horizan/horizan/RegisterActivity.java
@@ -1,15 +1,23 @@
 package com.horizan.horizan;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -50,6 +58,34 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void registerUser() {
-
+        final String firstName = firstNameEditText.getText().toString();
+        final String lastName = lastNameEditText.getText().toString();
+        final String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        if (TextUtils.isEmpty(firstName) || TextUtils.isEmpty(lastName) || TextUtils.isEmpty(email)
+                || TextUtils.isEmpty(password)) {
+            Toast.makeText(RegisterActivity.this, "There are one or more empty fields.",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                                String userId = firebaseUser.getUid();
+                                User user = new User(firstName, lastName, email);
+                                databaseReference.child("users").child(userId).setValue(user);
+                                Toast.makeText(RegisterActivity.this, "Registered Successfully",
+                                        Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(RegisterActivity.this, DashboardActivity.class));
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Failed to Register",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
