a5e3e3b0af0f6f64f7c15375eb7cfdbde08fd517
tejaskb04 <tejaskb@gmail.com>
2017-12-31 10:21:31 -0800

    Animation Logic

--- Horizan/app/src/main/java/com/horizan/horizan/LaunchActivity.java
+++ Horizan/app/src/main/java/com/horizan/horizan/LaunchActivity.java
@@ -4,21 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LaunchActivity extends AppCompatActivity {
 
-    private Button registerBtn;
-    private Button loginBtn;
-    private Button dashboardBtn;
+    //private Button registerBtn;
+    //private Button loginBtn;
+    //private Button dashboardBtn;
+    private TextView welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-        registerBtn = (Button) findViewById(R.id.register);
+        welcome = (TextView) findViewById(R.id.welcome);
+        /*registerBtn = (Button) findViewById(R.id.register);
         loginBtn = (Button) findViewById(R.id.login);
         dashboardBtn = (Button) findViewById(R.id.dashboard);
         registerBtn.setOnClickListener(new View.OnClickListener() {
@@ -38,10 +43,24 @@ public class LaunchActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(LaunchActivity.this, DashboardActivity.class));
             }
-        });
+        });*/
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            // Animation Logic
-            startActivity(new Intent(LaunchActivity.this, DashboardActivity.class));
+            Animation animation = AnimationUtils.loadAnimation(LaunchActivity.this, R.anim.launch_animation);
+            welcome.startAnimation(animation);
+            Thread timer = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        startActivity(new Intent(LaunchActivity.this, DashboardActivity.class));
+                        finish();
+                    }
+                }
+            };
+            timer.start();
         }
     }
 }
